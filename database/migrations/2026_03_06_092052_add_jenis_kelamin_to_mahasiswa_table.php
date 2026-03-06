dd519970ff6cb023eca735f0bbbfc3ba26fb3613
Intra-Sepriansa <intrasepriansaa@gmail.com>
2026-03-06 09:32:52 +0700

    fix(migration): change after prodi to after semester since prodi column does not exist

--- database/migrations/2026_03_06_092052_add_jenis_kelamin_to_mahasiswa_table.php
+++ database/migrations/2026_03_06_092052_add_jenis_kelamin_to_mahasiswa_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('mahasiswa', function (Blueprint $table) {
-            $table->enum('jenis_kelamin', ['L', 'P'])->nullable()->after('prodi');
+            $table->enum('jenis_kelamin', ['L', 'P'])->nullable()->after('semester');
         });
     }
 
